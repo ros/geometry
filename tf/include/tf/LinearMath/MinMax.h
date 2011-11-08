fc59abd4dee89fce64c4b8c038237623bb1d72ae
Tully Foote <tfoote@willowgarage.com>
2011-11-07 17:23:50 -0800

    deleting GEN_clamp and GEN_clamped as they're unused and would conflict

--- tf/include/tf/LinearMath/MinMax.h
+++ tf/include/tf/LinearMath/MinMax.h
@@ -29,11 +29,6 @@ SIMD_FORCE_INLINE const T& tfMax(const T& a, const T& b)
   return  a > b ? a : b;
 }
 
-template <class T>
-SIMD_FORCE_INLINE const T& GEN_clamped(const T& a, const T& lb, const T& ub) 
-{
-	return a < lb ? lb : (ub < a ? ub : a); 
-}
 
 template <class T>
 SIMD_FORCE_INLINE void tfSetMin(T& a, const T& b) 
@@ -53,17 +48,5 @@ SIMD_FORCE_INLINE void tfSetMax(T& a, const T& b)
 	}
 }
 
-template <class T>
-SIMD_FORCE_INLINE void GEN_clamp(T& a, const T& lb, const T& ub) 
-{
-	if (a < lb) 
-	{
-		a = lb; 
-	}
-	else if (ub < a) 
-	{
-		a = ub;
-	}
-}
 
 #endif

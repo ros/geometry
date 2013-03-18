d4559cfd3fde60b3cac328b4436a06fa7573a8ce
Jonathan Bohren <jbo@jhu.edu>
2013-03-18 17:00:45 -0300

    Removing scripts from setup.py install

--- tf/setup.py
+++ tf/setup.py
@@ -6,9 +6,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['tf'],
     package_dir={'': 'src'},
-    scripts=['scripts/bullet_migration_sed.py',
-             'scripts/tf_remap',
-             'scripts/view_frames'],
     requires=['genmsg', 'genpy', 'roslib', 'rospkg', 'geometry_msgs', 'sensor_msgs', 'std_msgs']
 )
 

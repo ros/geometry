72ece5e80749df1a544d0f03cf29b06adb0114d1
Tully Foote <tfoote@willowgarage.com>
2013-08-28 00:47:32 -0700

    fixing install rule syntax

--- tf/setup.py
+++ tf/setup.py
@@ -6,9 +6,9 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['tf'],
     package_dir={'': 'src'},
-    requires=['genmsg', 'genpy', 'roslib', 'rospkg', 'geometry_msgs', 'sensor_msgs', 'std_msgs']
-    scripts=['scripts/groovy_compatability/tf_remap',
-             'scripts/groovy_compatability/view_frames']
+    requires=['genmsg', 'genpy', 'roslib', 'rospkg', 'geometry_msgs', 'sensor_msgs', 'std_msgs'],
+    scripts=['scripts/groovy_compatibility/tf_remap',
+             'scripts/groovy_compatibility/view_frames']
 )
 
 setup(**d)

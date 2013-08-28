b1e643a0cca72ed5c83a2a615b1bbffb5ebcd273
Tully Foote <tfoote@willowgarage.com>
2013-08-28 00:40:19 -0700

    switching to wrapper scripts which will provide a deprecation warning for #3

--- tf/setup.py
+++ tf/setup.py
@@ -6,10 +6,9 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['tf'],
     package_dir={'': 'src'},
-    scripts=['scripts/bullet_migration_sed.py',
-             'scripts/tf_remap',
-             'scripts/view_frames'],
     requires=['genmsg', 'genpy', 'roslib', 'rospkg', 'geometry_msgs', 'sensor_msgs', 'std_msgs']
+    scripts=['scripts/groovy_compatability/tf_remap',
+             'scripts/groovy_compatability/view_frames']
 )
 
 setup(**d)
